1c4fb2b873b70d3186979ce528ac8c8a0366445c
Manezeu Patricia <pmanezeu@gmail.com>
2020-02-27 11:21:45 +0100

    Updates on bubble sort function

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,10 +1,10 @@
 def bubble_sort(arr)
     loop do
-        i = 0
+        i = 1
         test = false
-        while i < arr.length-1
-            if arr[i] > arr[i+1]
-                arr[i], arr[i+1] = arr[i+1], arr[i]
+        while i-1 < arr.length-1
+            if arr[i-1] > arr[i]
+                arr[i-1], arr[i] = arr[i], arr[i-1]
                 test = true
             end
             i += 1
@@ -12,4 +12,21 @@ def bubble_sort(arr)
         break if !test
     end
     return arr
+end
+
+def bubble_sort_by(arr) 
+    loop do 
+        i = 1 
+        test = false 
+        while i-1 < arr.length-1 
+            result = yield(arr[i -1], arr[i]) 
+            if result > 0 
+                arr[i - 1], arr[i] = arr[i], arr[i - 1] 
+                test = true 
+            end 
+            i += 1 
+        end 
+        break if !test 
+    end 
+    return arr 
 end
